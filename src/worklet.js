2ec954a69a03bd2e389c92d97e27ae030a835636
mosm <airman515@gmail.com>
2020-11-20 19:24:29 +0900

    More accurate velocity curve and envelope

--- src/worklet.js
+++ src/worklet.js
@@ -1,16 +1,17 @@
+const LEVEL_UNIT = (20 * Math.log10(2)) / 256; // approx. 0.0235 dB
+
 // prettier-ignore
-const ENV_OUTPUT_LEVELS = [
-    0, 5, 9, 13, 17, 20, 23, 25, 27, 29, 31, 33, 35, 37, 39,
-    41, 42, 43, 45, 46, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61,
-    62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80,
-    81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99,
-    100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114,
-    115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127
+const LEVEL_LUT = [
+    0, 5, 9, 13, 17, 20, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 42, 43, 45, 46
 ];
+
+function scaleOutLevel(outLevel) {
+    return outLevel >= 20 ? 28 + outLevel : LEVEL_LUT[outLevel];
+}
 
 const envOutputLUT = new Float32Array(4096);
 for (let i = 0; i < envOutputLUT.length; i++) {
-    const dB = (i - 3824) * 0.0235;
+    const dB = (i - 3824) * LEVEL_UNIT;
     envOutputLUT[i] = Math.pow(20, dB / 20);
 }
 
@@ -61,7 +62,7 @@ class Envelope {
             const newLevel = this._levels[this._state];
             this._targetLevel = Math.max(
                 0,
-                (ENV_OUTPUT_LEVELS[newLevel] << 5) - 224
+                (scaleOutLevel(newLevel) << 5) - 224
             );
             this._rising = this._targetLevel - this._level > 0;
             const qr = Math.min(63, (this._rates[this._state] * 41) >> 6);
@@ -71,28 +72,34 @@ class Envelope {
 }
 
 // prettier-ignore
-const OP_OUTPUT_LEVELS = [
-    0.000000, 0.000337, 0.000476, 0.000674, 0.000952, 0.001235, 0.001602, 0.001905, 0.002265, 0.002694,
-    0.003204, 0.003810, 0.004531, 0.005388, 0.006408, 0.007620, 0.008310, 0.009062, 0.010776, 0.011752,
-    0.013975, 0.015240, 0.016619, 0.018123, 0.019764, 0.021552, 0.023503, 0.025630, 0.027950, 0.030480,
-    0.033238, 0.036247, 0.039527, 0.043105, 0.047006, 0.051261, 0.055900, 0.060960, 0.066477, 0.072494,
-    0.079055, 0.086210, 0.094012, 0.102521, 0.111800, 0.121919, 0.132954, 0.144987, 0.158110, 0.172420,
-    0.188025, 0.205043, 0.223601, 0.243838, 0.265907, 0.289974, 0.316219, 0.344839, 0.376050, 0.410085,
-    0.447201, 0.487676, 0.531815, 0.579948, 0.632438, 0.689679, 0.752100, 0.820171, 0.894403, 0.975353,
-    1.063630, 1.159897, 1.264876, 1.379357, 1.504200, 1.640341, 1.788805, 1.950706, 2.127260, 2.319793,
-    2.529752, 2.758714, 3.008399, 3.280683, 3.577610, 3.901411, 4.254519, 4.639586, 5.059505, 5.517429,
-    6.016799, 6.561366, 7.155220, 7.802823, 8.509039, 9.279172, 10.11901, 11.03486, 12.03360, 13.12273
+const VELOCITY_DATA = [
+    0, 70, 86, 97, 106, 114, 121, 126, 132, 138, 142, 148, 152, 156, 160, 163,
+    166, 170, 173, 174, 178, 181, 184, 186, 189, 190, 194, 196, 198, 200, 202,
+    205, 206, 209, 211, 214, 216, 218, 220, 222, 224, 225, 227, 229, 230, 232,
+    233, 235, 237, 238, 240, 241, 242, 243, 244, 246, 246, 248, 249, 250, 251,
+    252, 253, 254
 ];
 
+function scaleVelocity(velocity, sensitivity) {
+    const clampedVel = Math.max(0, Math.min(127, velocity));
+    const velValue = VELOCITY_DATA[clampedVel >> 1] - 239;
+    const scaledVel = ((sensitivity * velValue + 7) >> 3) << 4;
+    return scaledVel;
+}
+
 class Operator {
-    constructor(params, baseFreq) {
+    constructor(params, baseFreq, velocity) {
         this.val = 0;
 
         this.ampL = Math.cos(((Math.PI / 2) * (params.pan + 50)) / 100);
         this.ampR = Math.sin(((Math.PI / 2) * (params.pan + 50)) / 100);
 
-        const idx = Math.min(99, Math.max(0, Math.floor(params.volume)));
-        this.outputLevel = OP_OUTPUT_LEVELS[idx] * 1.27;
+        let outLevel = scaleOutLevel(params.volume) << 5;
+        outLevel += scaleVelocity(velocity, params.velocitySens);
+        outLevel = Math.max(0, outLevel);
+
+        const outLeveldB = LEVEL_UNIT * (outLevel - 99 * 32);
+        this.outputLevel = Math.pow(10, outLeveldB / 20);
 
         this._phase = 0;
         this._envelope = new Envelope(params.levels, params.rates);
@@ -199,15 +206,12 @@ class Voice {
     constructor(note, velocity) {
         this.note = note;
         const freq = 440 * Math.pow(2, (note - 69) / 12);
-        velocity /= 127;
 
         this.down = true;
 
         this._operators = [];
         for (const param of PATCH.operators) {
-            const op = new Operator(param, freq);
-            op.outputLevel *= 1 + (velocity - 1) * (param.velocitySens / 7);
-            this._operators.push(op);
+            this._operators.push(new Operator(param, freq, velocity));
         }
     }
 
